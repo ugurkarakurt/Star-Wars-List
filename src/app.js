c01c342f94f57fe4055a8e6bd1374bee61c46b26
Uğur KARAKURT <ugurkarakurt57@gmail.com>
2020-10-18 17:54:36 +0300

    feat: Post request test

--- src/app.js
+++ src/app.js
@@ -9,4 +9,15 @@ request.get()
     .then(data => {
         console.log(data);
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
+request.post({
+    "name": "name",
+    "height": "height",
+    "mass": "mass",
+    "hair_color": "hair",
+    "skin_color": "skin",
+    "eye_color": "eye",
+    "birth_year": "birthDay",
+    "gender": "genderV"
+}).then(data=>console.log(data)
+)
--- src/request.js
+++ src/request.js
@@ -11,4 +11,15 @@ export class Request {
 
     }
 
+    async post(data) {
+        const response = await fetch(this.url, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        });
+        const responseData = await response.json()
+        return responseData;
+    }
 }
